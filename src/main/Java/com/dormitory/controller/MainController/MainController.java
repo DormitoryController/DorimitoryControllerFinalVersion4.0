7903a429c6e02a154acfc79d4aa7157c7e99dd7e
deadfish2 <526487921@qq.com>
2017-04-20 19:22:58 +0800

    this is the final DormitoryController model

---
+++ src/main/Java/com/dormitory/controller/MainController/MainController.java
@@ -0,0 +1,34 @@
+package com.dormitory.controller.MainController;
+
+import com.dormitory.Dao.UserDao;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.dormitory.model.po.User;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+/**
+ * Created by 赵嗣瑜 on 2017/4/19.
+ */
+@Controller
+public class MainController {
+    @Resource
+    private UserDao userDao;
+    @RequestMapping(value = "/")
+    public String index(){
+        System.out.println(userDao.addUser());
+        return "MainView/index";
+    }
+    @RequestMapping(value = "/user")
+    public String user(HttpSession httpSession){
+        List<User> userlist=userDao.findUser();
+        httpSession.setAttribute("userlist",userlist);
+        System.out.println(userDao.findUser());
+        return "MainView/user";
+    }
+    @RequestMapping(value ="/test" )
+    public String test(){
+        return "MainView/test";
+    }
+}
---
+++ src/main/webapp/View/MainView/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<form action="/user">
+    <input type="submit" value="submit"></form>
+</form>
+</body>
+</html>
