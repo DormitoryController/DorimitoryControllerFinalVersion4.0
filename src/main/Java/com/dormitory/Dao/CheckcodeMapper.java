60aac655569c33a61dcb7b4026c4eca0e37259c7
shinyhui <beasone233@163.com>
2017-05-15 00:27:15 +0800

    junit测试后版本2.2

--- src/main/Java/com/dormitory/Dao/CheckcodeMapper.java
+++ src/main/Java/com/dormitory/Dao/CheckcodeMapper.java
@@ -12,6 +12,7 @@ import java.util.List;
 @Repository
 public interface CheckcodeMapper {
     Checkcode selectCheckcodeById(int id);  //查询验证码
+    List<Checkcode> selectAllCheckcode();
     List<Checkcode> selectCheckcodeWhenRegister(String user_type);       //注册时根据用户类别查询验证码
     void updateCheckcodeState(@Param("id") int id,@Param("use_state") String use_state);    //修改验证码使用状态
     void insertCheckcode(Checkcode checkcode);             //插入验证码
